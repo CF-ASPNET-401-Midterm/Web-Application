5b287c92955a82cb7b48f6198d3e66e4fee16c22
AndrewBaik <andrewbaik@outlook.com>
2018-07-02 14:28:33 -0700

    Confirmed the connection with our API

---
+++ thePlayList/thePlayList/Models/PlaylistViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thePlayList.Models
+{
+    public class PlaylistViewModel : Controller
+    {
+        public List<Playlist> Playlists { get; set; }
+
+        public IEnumerable<Song> Songs { get; set; }
+    }
+}
